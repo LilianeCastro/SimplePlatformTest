d9733059a9926c117e06d10d119fe73feadd10d2
Liliane Castro <lcs2@icomp.ufam.edu.br>
2021-02-03 20:59:17 -0400

    feat: update

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] private Text _coinAmountText = default;
+    private int _coinAmount = 0;
+
+    [SerializeField] private ParticleSystem _feedbackCoinCollectedPrefab = default;
+
+    public void UpdateCoin()
+    {
+        _coinAmount++;
+        _coinAmountText.text = _coinAmount.ToString();
+    }
+
+    public void StartVfxCoinCollected(Transform coinTransform)
+    {
+        ParticleSystem feedbackTemp = Instantiate(_feedbackCoinCollectedPrefab, coinTransform.position, coinTransform.rotation);
+
+        Destroy(feedbackTemp.gameObject, 1.0f);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private GameController _gameController;
+    private Rigidbody2D _playerRb;
+
+    [Header("Jump")]
+    [SerializeField] private LayerMask _layerJumpMask = default;
+    [SerializeField] private Transform _groundCheckL = default;
+    [SerializeField] private Transform _groundCheckR = default;
+    [SerializeField] [Range(1.0f, 2.0f)] private float _distance = 1.0f;
+    [SerializeField] [Range(500.0f, 1000.0f)] private float _jumpForce = 800.0f;
+    private bool _isGrounded = true;
+
+    [SerializeField] [Range(1.0f, 10.0f)]private float _speedPlayer = 5.0f;
+    private float _horizontalInput = 0.0f;
+    
+    void Start()
+    {
+        _gameController = FindObjectOfType<GameController>();
+
+        _playerRb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Movement();
+        Jump();
+    }
+
+    private void FixedUpdate()
+    {
+        _isGrounded = Physics2D.Raycast(_groundCheckL.position, Vector2.down, _distance, _layerJumpMask)
+        || Physics2D.Raycast(_groundCheckR.position, Vector2.down, _distance, _layerJumpMask);
+    }
+
+    void Movement()
+    {
+        _horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        transform.Translate(Vector2.right * _horizontalInput * _speedPlayer * Time.deltaTime);
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && _isGrounded)
+        {
+            _playerRb.AddForce(Vector2.up * _jumpForce);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Coin"))
+        {
+            _gameController.UpdateCoin();
+            _gameController.StartVfxCoinCollected(other.transform);
+
+            Destroy(other.gameObject);
+        }
+    }
+}
